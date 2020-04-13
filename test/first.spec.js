5301a4b622b5e3cb4cf87ef3f4e2143eccdb32c3
ukashasohail <ukasha909@outlook.com>
2020-04-13 20:40:47 +0500

    init

---
+++ test/first.spec.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe("A feature", function(){
+    describe("a scenario", function(){
+        it("does something expected", function(){
+            assert(true);
+        });
+    });
+});
